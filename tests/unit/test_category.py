482bf2807a1c3fadd8a1424509832cce6eb128f1
tiventian <hjjtw@yahoo.com>
2023-01-27 14:48:42 +0800

    test category model

--- tests/unit/test_category.py
+++ tests/unit/test_category.py
@@ -7,3 +7,16 @@ def test_new_category(dbclient):
         assert Category.select().count() == 1
         c = Category.create(name='test', status='D')
         assert Category.select().count() == 2
+
+
+def test_update_category(dbclient):
+    with dbclient.app_context():
+        c = Category.create(name='python', status='N')
+        assert Category.select().count() == 1
+        c2 = Category.select().where(Category.name == 'python').first()
+        q = (Category
+             .update({Category.name: 'Python1', Category.status: 'D'})
+             .where(Category.name == 'python'))
+        q.execute()
+        assert Category.select().count() == 1
+        assert Category.get(Category.name == 'Python1').status == 'D'
